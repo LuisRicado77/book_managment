511ae799e49814a5e6e1ea77a29feefb8732bc28
LuisRicado77 <luissayas2018@gmail.com>
2024-08-29 23:59:24 -0500

    we have the files .env, mongo.ts and constants.ts united to connect to the database with mongo using mongoose

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express, { Request, Response } from "express";
 import { booksRouter } from "./modules/books/books.router";
 
+import { Parameter } from "./utils/constants";
+import { initDatabase } from "./db/mongo";
+
 //servicio de express
 const app = express();  
 
@@ -11,7 +14,11 @@ app.get("/", (req: Request, res: Response) => {
     res.status(200).send({ msg:"hello luis" });
 });
 
+//mongodb://localhost:27017/
+const url_database = "mongodb://"+Parameter.DATABASE_HOST+ ":"+Parameter.DATABASE_PORT+"/"+Parameter.DATABASE_NAME;
 
-app.listen(3000,() =>{
-    console.log("Server running at port 3000");
-})
+app.listen(3001, async()  =>{
+    console.log(url_database);
+    await initDatabase(url_database);
+    console.log("Server running at port 3001");
+});1
